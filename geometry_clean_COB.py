856380b01974be6af726b7b8bff2d974523b550a
Laurie Brès <lauriebres.lb@gmail.com>
2026-06-11 15:45:14 +0200

    Add files via upload

--- geometry_clean_COB.py
+++ geometry_clean_COB.py
@@ -2,12 +2,12 @@ from osgeo import gdal, ogr
 import geopandas as gpd
 import pandas as pd
 import fiona 
-gdf = gpd.read_file("C:\PANALESIS\Vers0_christian\COB_psAbs_output.gpkg")
-print(gdf)
-layers = fiona.listlayers("C:\PANALESIS\Vers0_christian\COB_psAbs_output.gpkg")
+gdf = gpd.read_file("C:\PANALESIS\outputs\COB_psAbs_output.gpkg")
+print(gdf.head)
+layers = fiona.listlayers("C:\PANALESIS\outputs\COB_psAbs_output.gpkg")
 for layer in layers :
-    gdf = gpd.read_file("C:\PANALESIS\Vers0_christian\COB_psAbs_output.gpkg", layer = layer)
-    print("layer :",layer)
+    gdf = gpd.read_file("C:\PANALESIS\outputs\COB_psAbs_output.gpkg", layer = layer)
+    print("layer :", layer)
     print(gdf)
 
 
@@ -15,14 +15,28 @@ print(gdf.columns)
 print(gdf.geom_type.unique()) 
 
 for layer in layers :
-    gdf = gpd.read_file("C:\PANALESIS\Vers0_christian\COB_psAbs_output.gpkg", layer = layer)
+    gdf = gpd.read_file("C:\PANALESIS\outputs\COB_psAbs_output.gpkg", layer = layer)
     print(gdf.geometry)
     
     
-# Identifier les géometries empty (True = empty) pour toutes les couches du gpkg
+# Identifier les géometries empty pour toutes les couches du gpkg
 
 for layer in layers :
-    gdf = gpd.read_file("C:\PANALESIS\Vers0_christian\COB_psAbs_output.gpkg", layer = layer)
-    print(gdf.geometry.is_empty)
+    gdf = gpd.read_file("C:\PANALESIS\outputs\COB_psAbs_output.gpkg", layer = layer)
+    if gdf.geometry.is_empty.any() :
+        print("layer:", layer)
+        print(gdf[gdf.geometry.is_empty])
+    else :
+        print ("layer:", layer, "pas de géométrie vide")
     
 #Pas de géométrie empty pour la COB
+
+# Identifier si valeurs manquantes pour les géométries (NA) pour toutes les couches du gpkg :
+
+for layer in layers :
+    gdf = gpd.read_file("C:\PANALESIS\outputs\COB_psAbs_output.gpkg", layer = layer)
+    if gdf.geometry.isna().any() :
+        print("layer:", layer)
+        print(gdf[gdf.geometry.isna()])
+    else :
+        print ("layer:", layer, "pas de géométrie NA")
--- geometry_clean_plates.py
+++ geometry_clean_plates.py
@@ -2,75 +2,64 @@ from osgeo import gdal, ogr
 import geopandas as gpd
 import pandas as pd
 import fiona 
-gdf = gpd.read_file("C:\PANALESIS\Vers0_christian\Plates_psAbs_output.gpkg")
-print(gdf)
-layers = fiona.listlayers("C:\PANALESIS\Vers0_christian\Plates_psAbs_output.gpkg")
+gdf = gpd.read_file("C:\PANALESIS\outputs\Plates_psAbs_output.gpkg")
+print(gdf.head)
+layers = fiona.listlayers("C:\PANALESIS\outputs\Plates_psAbs_output.gpkg")
 for layer in layers :
-    gdf = gpd.read_file("C:\PANALESIS\Vers0_christian\Plates_psAbs_output.gpkg", layer = layer)
+    gdf = gpd.read_file("C:\PANALESIS\outputs\Plates_psAbs_output.gpkg", layer = layer)
     print("layer:", layer)
-    print(gdf)
+    print(gdf.head)
 
 
 print(gdf.columns)
 print(gdf.geom_type.unique()) 
 
 for layer in layers :
-    gdf = gpd.read_file("C:\PANALESIS\Vers0_christian\Plates_psAbs_output.gpkg", layer = layer)
+    gdf = gpd.read_file("C:\PANALESIS\outputs\Plates_psAbs_output.gpkg", layer = layer)
     print(gdf.geometry)
 
-    
-# Identifier les géometries empty (True = empty) pour toutes les couches du gpkg
+# Identifier les géometries empty pour toutes les couches du gpkg
 
 for layer in layers :
-    gdf = gpd.read_file("C:\PANALESIS\Vers0_christian\Plates_psAbs_output.gpkg", layer = layer)
-    print(gdf.geometry.is_empty)
-    print(gdf.geometry.notna())
-    
-    
-#Afficher uniquement les lignes avec des geométries empty avec le nom de la couche, et leur index :
-
-geom_empty = []
-for layer in layers :
-    gdf = gpd.read_file("C:\PANALESIS\Vers0_christian\Plates_psAbs_output.gpkg", layer = layer)
-    empty_rows = gdf[gdf.geometry.is_empty]
-    print(gdf.geometry.is_empty.sum())
-    for idx, row in empty_rows.iterrows():
-        geom_empty.append({
-            "layer": layer,
-            "index": idx,
-            "geometry": str(row.geometry)
-        })
-    
+    gdf = gpd.read_file("C:\PANALESIS\outputs\Plates_psAbs_output.gpkg", layer = layer)
+    if gdf.geometry.is_empty.any() :
+        print("layer:", layer)
+        print(gdf[gdf.geometry.is_empty])
+    else :
+        print ("layer:", layer, "pas de géométrie vide")
         
-geom_empty_df = pd.DataFrame(geom_empty)
-print(geom_empty_df)
+# Pas de géométrie empty pour les plates
+        
+        
+# Identifier si valeurs manquantes pour les géométries (NA) pour toutes les couches du gpkg :
 
-
-#Pas de géométrie empty pour les plates
-
-# Check géométrie na
 for layer in layers :
-    gdf = gpd.read_file("C:\PANALESIS\Vers0_christian\Plates_psAbs_output.gpkg", layer = layer)
-    print(gdf.geometry.isna())
-    
-# Il y a une geometrie na 
+    gdf = gpd.read_file("C:\PANALESIS\outputs\Plates_psAbs_output.gpkg", layer = layer)
+    if gdf.geometry.isna().any() :
+        print("layer:", layer)
+        print(gdf[gdf.geometry.isna()])
+    else :
+        print ("layer:", layer, "pas de géométrie NA")
+        
+# Une géométrie NA (age : 535)
 
-# Export : 
-output_gpkg = r"C:\PANALESIS\Vers0_christian\Plates_psAbs_output_geom_clean.gpkg"
+# Export fichier clean : 
+output_gpkg = r"C:\PANALESIS\outputs\Plates_psAbs_output_geom_clean.gpkg"
 for layer in layers :
-    gdf = gpd.read_file("C:\PANALESIS\Vers0_christian\Plates_psAbs_output.gpkg", layer = layer)
+    gdf = gpd.read_file("C:\PANALESIS\outputs\Plates_psAbs_output.gpkg", layer = layer)
     gdf_clean = gdf[gdf.geometry.notna() &
         ~gdf.geometry.is_empty]# "~"" selectionne les geom pas vides
     gdf_clean.to_file(output_gpkg, layer=layer, driver="GPKG")
     
 print("Export terminé")
 
-#Verification : 
+# Vérification 
 
+layers = fiona.listlayers("C:\PANALESIS\outputs\Plates_psAbs_output_geom_clean.gpkg")
 for layer in layers :
-    gdf = gpd.read_file("C:\PANALESIS\Vers0_christian\Plates_psAbs_output_geom_clean.gpkg", layer = layer)
-    print(gdf.geometry.is_empty.sum())
-    
-for layer in layers :
-    gdf = gpd.read_file("C:\PANALESIS\Vers0_christian\Plates_psAbs_output_geom_clean.gpkg", layer = layer)
-    print(gdf.geometry.isna)
+    gdf = gpd.read_file("C:\PANALESIS\outputs\Plates_psAbs_output_geom_clean.gpkg", layer = layer)
+    if gdf.geometry.isna().any() :
+        print("layer:", layer)
+        print(gdf[gdf.geometry.isna()])
+    else :
+        print ("layer:", layer, "pas de géométrie NA")
